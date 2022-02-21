5fbd3773856a752f3d09a2d7532f9c8012333711
Patchapong <patchapong.kul@gmail.com>
2022-02-21 17:01:08 +0700

    add table manager bash script

---
+++ table_mange.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "This is Table Manager Script"
+echo "Please choose following commands:"
+echo ""
+echo "1 See table"
+echo "2 list column"
+echo "3 Sort table"
+echo "4 Modify table"
+echo ""
+echo "other: exit"
+echo -n "Please choose the commands(1-4): "
+read cmd
+
+if [ "$cmd" == "1" ]; then
+	cat table.txt
+elif [ "$cmd" == "2" ]; then
+	cat table.txt | head -n1
+	echo -n "List Column: "
+	read col
+	awk -v var=$col '{print $var}' table.txt
+elif [ "$cmd" == "3" ]; then
+	cat table.txt | head -n1
+	echo -n "Sort Column: "
+	read col
+	tail -n +2 table.txt | sort -k$col
+elif [ "$cmd" == "4" ]; then
+	cat table.txt | head -n1
+	echo -n "Change from: "
+	read previous
+	echo -n "To: "
+	read new
+	sed "s/$previous/$new/" table.txt > new_table.txt
+	cat new_table.txt
+else
+	echo "Exiting Script"
+fi
