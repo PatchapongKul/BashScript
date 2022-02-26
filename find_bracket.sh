cab0f9b117bab6e3bbf08de8b4fb46ad15e58c7e
Patchapong <patchapong.kul@gmail.com>
2022-02-26 20:39:16 +0700

    fix bug by replacing grep with sed

--- find_bracket.sh
+++ find_bracket.sh
@@ -12,7 +12,7 @@ echo -ne "\nDo you want to remove brackets into new file? (y/n): "
 read cmd
 
 if [ "$cmd" == "y" ];then
-	export newfile="no_brac_${file}"
-	grep -viP '\[.*?\]' $file > $newfile
+	export newfile="no_brac_$file"
+	cat $file | sed -E "s/\[[0123456789\]]/ /" > $newfile
 	cat $newfile
 fi
